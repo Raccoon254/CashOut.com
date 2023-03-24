cea20e74c2420cc5f9a90366d3ee3b88a066dc80
Steve Tom <tomsteve187@gmail.com>
2023-03-24 13:56:59 +0300

    Edited the database config data

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "cash";
+$dbname = "cashoutold";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
